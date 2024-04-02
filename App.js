c9a94bb3ed2b15eb97c254eb473bf5cc578894a5
victor <victorhugomotabueno@gmail.com>
2024-04-02 11:25:31 -0300

    Notificações Locais

--- App.js
+++ App.js
@@ -1,12 +1,85 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, Button } from 'react-native';
+
+/* Importando TODOS os recursos para notificação  */
+import * as Notifications from "expo-notifications";
+
+import { useState, useEffect } from 'react';
+
+/* Manipulador de eventos de notificações */
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldShowAlert: true,
+      shouldPlaySound: true,
+      shouldSetBadge: true,
+    };
+  },
+});
 
 export default function App() {
+  const [dados, setDados] = useState(null);
+
+  useEffect(() => {
+    /* Verificações/configurações de permissões de notificação
+    exclusivas para iOS */
+    async function permissoesIos() {
+      return await Notifications.requestPermissionsAsync({
+        ios: {
+          allowAlert: true,
+          allowSound: true,
+          allowBadge: true,
+        }
+      })
+    }
+    permissoesIos();
+
+    /* Ouvindo de evento para notificações recebidas, ou seja, quando a notificação
+    aparece no topo do app. */
+    Notifications.addNotificationReceivedListener(notificacao => {
+      console.log(notificacao);
+    });
+
+    /* Ouvinte de evento para as respostas dadas às notificações, 
+    ou seja, quando o usuário interage (toca) notifiacção. */
+    Notifications.addNotificationResponseReceivedListener((resposta) => {
+      setDados(resposta.notification.request.content.data);
+    })
+  }, [])
+  const enviarMensagem = async () => {
+    /* Montando a mensagem que será enviada via sistema de notificação LOCAL */
+    const mensagem = {
+      title: "Lembrete! 😛",
+      body: "Não se esqueça de estudar muito... senão, reprova! 😈",
+      data: {
+        usuario: "Chapolin Colorado",
+        cidade: "São Paulo"
+      }
+    }
+
+    /* Função de agendamento de notificaões */
+    await Notifications.scheduleNotificationAsync({
+      // Conteudo da notificação
+      content: mensagem,
+
+      // acionador/disparador da notificação
+      trigger: { seconds: 5 },
+    })
+  };
   return (
-    <View style={styles.container}>
-      <Text>Exemplo de Notificações!</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <Text>Exemplo de Notificações!</Text>
+        <Button title='Disparar Notificações' onPress={enviarMensagem} />
+
+        {dados && (
+          <View style={{ marginVertical: 8, backgroundColor: "yellow" }}>
+            <Text>Usuario: {dados.usuario} </Text>
+            <Text>Cidade: {dados.cidade} </Text>
+          </View>
+        )}
+      </View>
+    </>
   );
 }
 
